eebcf928f27ca06c820ece681fa3c37e86c959ce
Noah <33094578+coolreader18@users.noreply.github.com>
2020-04-09 20:23:02 -0500

    Reduce startup time a bit for the python stdlib

--- src/logic/pyrunner/stdlib.py
+++ src/logic/pyrunner/stdlib.py
@@ -1,26 +1,36 @@
-from collections import namedtuple
 import math
 import enum
 
 from random import randrange
 
-class Coord(namedtuple("Coord", "x y")):
-  def distance(self, other):
-    return math.sqrt((other.x - self.x)**2 + (other.y - self.y)**2)
-  def walking_distance(self, other):
-    return abs(other.x - self.x) + abs(other.y - self.y)
-  def coords_around(self, other):
-    pass 
-  def toward(self, other):
-    pass  
+
+class Coord:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self, other):
+        return math.sqrt((other.x - self.x) ** 2 + (other.y - self.y) ** 2)
+
+    def walking_distance(self, other):
+        return abs(other.x - self.x) + abs(other.y - self.y)
+
+    def coords_around(self, other):
+        pass
+
+    def toward(self, other):
+        pass
+
 
 class Team(enum.Enum):
     Red = "Red"
     Blue = "Blue"
 
+
 class Obj:
     def __init__(self, objdict):
         self.__obj = objdict
+
 
 class State:
     def __init__(self, statedict):
@@ -31,7 +41,7 @@ class State:
             self.other_team = Team.Blue
         else:
             self.other_team = Team.Red
-    
+
     def obj_by_id(self, id):
         return Obj(self.__state["objs"][id])
 
@@ -51,14 +61,19 @@ class State:
         xs = self.__state["grid"][coord.x]
         return xs and xs[coord.y]
 
+
 class ActionType(enum.Enum):
     Attack = "Attack"
     Move = "Move"
+
+
 class Direction(enum.Enum):
     North = "North"
     South = "South"
     East = "East"
     West = "West"
+
+
 class Action:
     def __init__(self, type, direction):
         if not isinstance(type, ActionType):
@@ -67,31 +82,45 @@ class Action:
             raise TypeError("direction must be a Direction")
         self.type = type
         self.direction = direction
+
     def __repr__(self):
         return f"<Action: {self.type} {self.direction}"
- 
+
+
 def move(direction):
     return Action(ActionType.Move, direction)
+
+
 def attack(direction):
     return Action(ActionType.Attack, direction)
 
+
 import io
+
+
 class _RobotRumbleLoggingIO(io.TextIOBase):
     def __init__(self, log):
         self._log = log
+
     def write(self, s):
         if self._log:
             self._log(s)
         else:
-            raise RuntimeError("unable to print; the current runner does not support logging")
+            raise RuntimeError(
+                "unable to print; the current runner does not support logging"
+            )
         return len(s)
+
     def flush(self):
         pass
+
+
 del io
 
- 
+
 def _main(state, log=None):
     import sys
+
     sys.stdout = sys.stderr = _RobotRumbleLoggingIO(log)
 
     state = State(state)
@@ -99,8 +128,10 @@ def _main(state, log=None):
     if not isinstance(robot, type(_main)):
         raise TypeError("you must define a 'robot' function")
     if robot.__code__.co_argcount != 2:
-        raise TypeError("your robot function must accept 2 values: the current state \
-                         and unit details")
+        raise TypeError(
+            "your robot function must accept 2 values: the current state "
+            "and the details for the unit"
+        )
     output = {}
     for id in state.ids_by_team(state.team):
         action = robot(state, state.obj_by_id(id))
