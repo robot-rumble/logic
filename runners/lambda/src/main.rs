19df124771068df252c67ed65b5ddc722dd7b8e7
Noah <33094578+coolreader18@users.noreply.github.com>
2020-04-25 00:31:33 -0500

    Add wasmer running to the lambda runner

--- runners/lambda/src/main.rs
+++ runners/lambda/src/main.rs
@@ -1,11 +1,21 @@
 #![allow(non_snake_case)]
 
-use lambda::handler_fn;
 use rusoto_core::Region;
 use rusoto_sqs::{SendMessageRequest, Sqs, SqsClient};
+
+use std::cell::Cell;
+
+use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use std::collections::HashMap;
+
+use logic::{ProgramError, Team};
+use native_runner::TokioRunner;
+use tokio::time::{self, Duration, Instant};
+use tokio::{io, task};
+
+use wasi_runner::WasiProcess;
+use wasmer_runtime::Module;
+use wasmer_wasi::{state::WasiState, WasiVersion};
 
 /*
 SAMPLE EVENT
@@ -67,38 +77,120 @@ struct Output {
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let func = handler_fn(handler);
-    lambda::run(func).await
+static PYRUNNER: Lazy<(Module, WasiVersion)> = Lazy::new(|| {
+    let pyrunner_wasm = include_bytes!("../../../target/wasm32-wasi/release/pyrunner.wasm");
+    let module = wasmer_runtime::compile(pyrunner_wasm).expect("pyrunner is invalid wasm");
+    let version = wasmer_wasi::get_wasi_version(&module, true).unwrap();
+    (module, version)
+});
+
+fn make_state(code: String) -> WasiState {
+    use wasmer_wasi::state;
+    let code = Cell::new(code);
+    let mut state = WasiState::new("robot");
+    wasi_runner::add_stdio(&mut state);
+    state.arg("/sourcecode").setup_fs(Box::new(move |fs| {
+        let fd = fs
+            .open_file_at(
+                state::VIRTUAL_ROOT_FD,
+                Box::new(state::Stdin),
+                0,
+                "sourcecode".to_owned(),
+                state::ALL_RIGHTS,
+                state::ALL_RIGHTS,
+                0x10, // all rights, all bits 0..=4
+            )
+            .unwrap();
+        let code = code.take();
+        let kind = state::Kind::Buffer {
+            buffer: code.into_bytes(),
+        };
+        let stat = fs.get_stat_for_kind(&kind).unwrap();
+        let inode = fs.get_inodeval_mut(fd).unwrap();
+        inode.stat = stat;
+        inode.kind = kind;
+        Ok(())
+    }));
+    state.build().unwrap()
 }
 
-async fn handler(event: Value) -> Result<(), Error> {
-    println!("INPUT: {}", event);
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let func = lambda::handler_fn(run);
+    lambda::run(func).await?;
+    Ok(())
+}
 
-    let lambda_input = serde_json::from_value::<LambdaInput>(event)?;
-    let input_data = serde_json::from_str::<Input>(&lambda_input.Records[0].body)?;
+async fn run(data: LambdaInput) -> Result<(), Error> {
+    let input_data = serde_json::from_str::<Input>(&data.Records[0].body).unwrap();
 
-    let data = logic::MainOutput {
-        winner: None,
-        errors: HashMap::new(),
-        turns: Vec::new(),
+    let make_runner = |code| async move {
+        let (ref module, version) = *PYRUNNER;
+        let state = make_state(code);
+        let imports = wasmer_wasi::generate_import_object_from_state(state, version);
+        let instance = module.instantiate(&imports).unwrap();
+        let mut proc = WasiProcess::spawn(instance);
+        let stdin = io::BufWriter::new(proc.take_stdin().unwrap());
+        let stdout = io::BufReader::new(proc.take_stdout().unwrap());
+        proc.take_stderr();
+        let t = task::spawn(async move {
+            let start_t = Instant::now();
+            let res = match time::timeout(TIMEOUT, proc).await {
+                Ok(Ok(())) => Ok(()),
+                Ok(Err(_wasm_err)) => Err(ProgramError::InternalError),
+                Err(_timeout) => Err(ProgramError::Timeout(TIMEOUT)),
+            };
+            (start_t.elapsed(), res)
+        });
+        (TokioRunner::new(stdin, stdout).await, t)
+    };
+
+    let ((r1, t1), (r2, t2)) = tokio::join!(
+        make_runner(input_data.r1_code),
+        make_runner(input_data.r2_code),
+    );
+
+    let run_fut = logic::run(r1, r2, |_| {}, TURN_COUNT);
+
+    let (mut output, err1, err2) = tokio::join!(run_fut, t1, t2);
+
+    let mut handle_res = |team, res: Result<_, task::JoinError>| match res {
+        Ok((dur, res)) => {
+            if let Err(e) = res {
+                output.errors.insert(team, e);
+            }
+            Duration::as_secs_f64(&dur)
+        }
+        Err(_) => {
+            output.errors.insert(team, ProgramError::InternalError);
+            -1.0
+        }
+    };
+    let r1_time = handle_res(Team::Red, err1);
+    let r2_time = handle_res(Team::Blue, err2);
+
+    let data = serde_json::to_string(&output)?;
+    let winner = match output.winner {
+        Some(Team::Red) => Winner::R1,
+        Some(Team::Blue) => Winner::R2,
+        None => Winner::Draw,
+    };
+    let errored = !output.errors.is_empty();
+
+    let output = Output {
+        r1_time,
+        r1_id: input_data.r1_id,
+        r2_time,
+        r2_id: input_data.r2_id,
+        data,
+        winner,
+        errored,
     };
 
     let client = SqsClient::new(Region::UsEast1);
 
     let out_queue_url = std::env::var("BATTLE_QUEUE_OUT_URL")
         .expect("\"BATTLE_QUEUE_OUT_URL\" environmental variable not found");
-
-    let output = Output {
-        r1_time: 0.,
-        r1_id: input_data.r1_id,
-        r2_time: 0.,
-        r2_id: input_data.r2_id,
-        data: serde_json::to_string(&data)?,
-        winner: Winner::Draw,
-        errored: false,
-    };
 
     client
         .send_message(SendMessageRequest {
@@ -114,3 +206,6 @@ async fn handler(event: Value) -> Result<(), Error> {
 
     Ok(())
 }
+
+const TIMEOUT: Duration = Duration::from_secs(60 * 3);
+const TURN_COUNT: usize = 10;
