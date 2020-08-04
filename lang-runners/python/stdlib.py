25ebc35ff5eb751b0531c374a26902e8c572ffdd
Anton <antonoutkine@gmail.com>
2020-08-04 12:42:18 -0500

    Change python import namespacing and delete all imports after their use

--- lang-runners/python/stdlib.py
+++ lang-runners/python/stdlib.py
@@ -2,7 +2,6 @@
 import enum
 import typing
 
-
 def check_instance(val: typing.Any, cls: typing.Any, func_name: str):
     if not isinstance(val, cls):
         raise TypeError(f"{func_name} argument must be an instance of {cls.__name__}")
