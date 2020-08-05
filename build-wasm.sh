19a178e08407f5b2cdbb10d8c9e940f09af13c33
Anton <antonoutkine@gmail.com>
2020-08-04 20:23:30 -0500

    Improve build-wasm.sh script
    Allow for building the langs separately.

--- build-wasm.sh
+++ build-wasm.sh
@@ -10,11 +10,15 @@ mkdir -p "$OUTDIR"
 OPTIMIZE=
 LANGS=
 BROWSER=
+PYTHON=
+JAVASCRIPT=
 for arg in "$@"; do
     case "$arg" in
         --optimize) OPTIMIZE=1 ;;
         --langs) LANGS=1 ;;
         --browser) BROWSER=1 ;;
+        --python) PYTHON=1 ;;
+        --javascript) JAVASCRIPT=1 ;;
         --all)
             LANGS=1
             BROWSER=1
@@ -22,24 +26,10 @@ for arg in "$@"; do
     esac
 done
 
-if [[ ! $LANGS && ! $BROWSER ]]; then
+if [[ ! $LANGS && ! $BROWSER && ! $PYTHON && ! $JAVASCRIPT ]]; then
     echo "No build targets selected, exiting..."
     exit
 fi
-
-copy_lang() {
-    fname=$(basename "$1")
-
-    if [[ $OPTIMIZE ]]; then
-        # Do this in order to work around some weird parsing bug in wasm-opt
-        wasm-dis "$1" -o "$fname.wat"
-        wasm-opt "$fname.wat" -Os -o "$1"
-        rm "$fname.wat"
-    fi
-
-    mkdir -p "$OUTDIR/lang-runners"
-    cp "$1" "$OUTDIR/lang-runners/$fname"
-}
 
 BOLD=$(printf '\033[1m')
 NC=$(printf '\033[0m')
@@ -56,26 +46,54 @@ prepend() {
 
 pids=()
 
-if [[ $BROWSER ]]; then
+build_browser() {
     {
         wasm-pack build env-runners/browser
         cp -rT env-runners/browser/pkg "$OUTDIR/browser-runner"
     } 2>&1 | prepend browser-runner: &
     pids+=($!)
-fi
+}
 
-if [[ $LANGS ]]; then
+copy_lang() {
+    fname=$(basename "$1")
+
+    if [[ $OPTIMIZE ]]; then
+        # Do this in order to work around some weird parsing bug in wasm-opt
+        wasm-dis "$1" -o "$fname.wat"
+        wasm-opt "$fname.wat" -Os -o "$1"
+        rm "$fname.wat"
+    fi
+
+    mkdir -p "$OUTDIR/lang-runners"
+    cp "$1" "$OUTDIR/lang-runners/$fname"
+}
+
+build_javascript() {
     {
         make -C lang-runners/javascript
         copy_lang lang-runners/javascript/jsrunner.wasm
     } 2>&1 | prepend "$JSYELLOWBG$JSGRAY"jsrunner: &
     pids+=($!)
+}
 
+build_python() {
     {
         cargo build --release --target wasm32-wasi --manifest-path=lang-runners/python/Cargo.toml
         copy_lang target/wasm32-wasi/release/pyrunner.wasm
     } 2>&1 | prepend "$PYBLUEBG$PYYELLOW"pyrunner: &
     pids+=($!)
+}
+
+if [[ $BROWSER ]]; then
+    build_browser
+fi
+
+if [[ $LANGS ]]; then
+    build_javascript
+    build_python
+else
+    [[ $PYTHON ]] && build_python
+    [[ $JAVASCRIPT ]] && build_javascript
 fi
 
 code=0
