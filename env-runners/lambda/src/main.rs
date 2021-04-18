d4cf249fafb1e0720076596aed37295583249e4f
Anton <antonoutkine@gmail.com>
2021-04-17 21:24:41 -0500

    Fix lambdan

--- env-runners/lambda/src/main.rs
+++ env-runners/lambda/src/main.rs
@@ -158,7 +158,7 @@ fn make_state(code: &str) -> (WasiState, tempfile::TempDir) {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let _sentry = sentry::init(&std::env::var("SENTRY_DSN"));
+    let _sentry = sentry::init(std::env::var("SENTRY_DSN").unwrap());
     let func = lambda::handler_fn(run);
     lambda::run(func).await?;
     Ok(())
