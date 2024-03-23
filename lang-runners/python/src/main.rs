2d0e5e89ba3712eeb8bf89e940389bcf084cab0b
Anton <antonoutkine@gmail.com>
2024-03-23 14:21:39 -0500

    Update rustpython

--- lang-runners/python/src/main.rs
+++ lang-runners/python/src/main.rs
@@ -1,28 +1,30 @@
 use rustpython_vm::builtins::PyDictRef;
+use rustpython_vm::object::{PyObjectRef, PyResult};
 use rustpython_vm::py_compile;
 use rustpython_vm::py_serde;
-use rustpython_vm::pyobject::{ItemProtocol, PyObjectRef, PyResult};
 use rustpython_vm::scope::Scope;
-use rustpython_vm::{InitParameter, Interpreter, PySettings, VirtualMachine};
+use rustpython_vm::Interpreter;
+use rustpython_vm::VirtualMachine;
 
 use logic::{ProgramError, ProgramResult};
 
 fn setup_scope(vm: &VirtualMachine) -> PyDictRef {
-    let code = vm.new_code_object(py_compile!(
+    let code = vm.ctx.new_code(py_compile!(
         file = "stdlib/rumblelib.py",
         module_name = "rumblelib"
     ));
 
     let attrs = vm.ctx.new_dict();
     let run = || -> PyResult<()> {
-        attrs.set_item("__name__", vm.ctx.new_str("<robot>".to_owned()), vm)?;
+        attrs.set_item("__name__", vm.ctx.new_str("<robot>".to_owned()).into(), vm)?;
         vm.run_code_obj(code, Scope::with_builtins(None, attrs.clone(), vm))?;
         let sys_modules: PyDictRef = vm
-            .get_attribute(vm.sys_module.clone(), "modules")?
+            .get_attribute_opt(vm.sys_module.clone().into(), "modules")?
+            .unwrap()
             .downcast()
             .ok()
             .expect("sys.modules should be dict");
-        sys_modules.set_item("rumblelib", attrs.clone().into_object(), vm)?;
+        sys_modules.set_item("rumblelib", attrs.clone().into(), vm)?;
         Ok(())
     };
     vm.unwrap_pyresult(run());
@@ -40,29 +42,33 @@ fn py_to_serde<T: serde::de::DeserializeOwned>(
 
 fn invoke_main(main: &PyObjectRef, input: serde_json::Value, vm: &VirtualMachine) -> ProgramResult {
     let input = py_serde::deserialize(vm, input)?;
-    let ret = vm.invoke(main, vec![input]).map_err(|e| {
+    let ret = main.call(vec![input], vm).map_err(|e| {
         eprintln!("error in stdlib init:");
-        rustpython_vm::exceptions::print_exception(vm, e);
+        vm.print_exception(e);
         ProgramError::InternalError
     })?;
     py_to_serde(&ret, vm).and_then(|r| r)
 }
 
 fn __init(code: &str) -> ProgramResult<impl FnMut(serde_json::Value) -> ProgramResult> {
-    let interp = Interpreter::new(PySettings::default(), InitParameter::Internal);
+    let interp = Interpreter::with_init(Default::default(), |vm| {
+        vm.add_native_modules(rustpython_stdlib::get_module_inits());
+        vm.add_frozen(rustpython_pylib::FROZEN_STDLIB);
+    });
+
     let main = interp.enter(|vm| {
         let code = vm
             .compile(
                 code,
-                rustpython_vm::compile::Mode::Exec,
+                rustpython_vm::compiler::Mode::Exec,
                 "<robot>".to_owned(),
             )
             .map_err(|err| {
                 ProgramError::InitError(logic::Error {
                     summary: err.to_string(),
                     details: None,
-                    loc: Some(logic::ErrorLoc {
-                        start: (err.location.row(), Some(err.location.column())),
+                    loc: err.location.map(|loc| logic::ErrorLoc {
+                        start: (loc.row.to_usize(), Some(loc.column.to_usize())),
                         end: None,
                     }),
                 })
@@ -84,8 +90,8 @@ fn __init(code: &str) -> ProgramResult<impl FnMut(serde_json::Value) -> ProgramR
             Err(exc) => {
                 // if setup errors, try to format the error, and just return an InternalError if it
                 // doesn't work
-                let exc = vm
-                    .invoke(&formatexc, vec![exc.into_object()])
+                let exc = formatexc
+                    .call(vec![exc.into()], vm)
                     .map_err(|_| ProgramError::InternalError)?;
                 let err = py_to_serde(&exc, &vm).map_err(|_| ProgramError::InternalError)?;
                 Err(ProgramError::InitError(err))
